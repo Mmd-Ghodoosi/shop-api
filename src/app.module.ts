d8516dcfe726ec273be4cedf770619f0551dc8f4
mmd-ghodoosi <mamadakgh@gmail.com>
2024-05-17 23:43:26 +0330

    added env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 const cookie = require('cookie-session');
 
 @Module({
   imports: [
     UsersModule,
     ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     MongooseModule.forRoot('mongodb://0.0.0.0:27017/shop-api'),
   ],
   controllers: [AppController],
@@ -20,7 +25,8 @@ const cookie = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private config: ConfigService) {}
   configure(consume: MiddlewareConsumer) {
-    consume.apply(cookie({ keys: ['asdf'] })).forRoutes('*');
+    consume.apply(cookie({ keys: [this.config.get('COOKIE')] })).forRoutes('*');
   }
 }
